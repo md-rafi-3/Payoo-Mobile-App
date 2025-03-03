63591a0be653a69d6d9ef7cf1eedd43bef626043
md-rafi-3-4 <mdrafiislam9411@gmail.com>
2025-03-04 00:24:42 +0600

    updated

--- script/add_money.js
+++ script/add_money.js
@@ -68,4 +68,104 @@ document.getElementById('add-money').addEventListener('click',function(event){
         alert("Invalid Number")
     }
 
-})
+});
+
+
+// cash transfer
+
+document.getElementById('send').addEventListener('click',function(event){
+    event.preventDefault();
+    const transferAmount = document.getElementById('transfer-amount').value;
+    const convertedTransferAmount = parseFloat(transferAmount);
+    console.log(convertedTransferAmount);
+   
+    const number = document.getElementById('user-number').value;
+
+    const pin = document.getElementById('transfer-password').value;
+
+    const balance = document.getElementById('main-balance').innerText;
+
+    const convertedBalance = parseInt(balance);
+
+    
+
+    if(number === "01618802042"){
+        if(parseInt(pin)=== 1234){
+            const minus = convertedBalance - convertedTransferAmount;
+          
+            document.getElementById('main-balance').innerText = minus;
+            
+        }
+        else{
+            alert("Invalid Password");  
+        }
+    }
+
+    else{
+        alert("Invalid Number")
+    }
+
+
+    
+});
+
+
+// get bonus
+document.getElementById('bonus').addEventListener('click',function(event){
+    event.preventDefault();
+
+    const bonusValue =document.getElementById('bonus-coupon').value;
+
+    const mainBalance = document.getElementById('main-balance').innerText;
+     const convertedBalance = parseFloat(mainBalance);
+    if (bonusValue === '500 tk vikkha dau'){
+       const bonus = convertedBalance  + 500;
+       document.getElementById('main-balance').innerText = bonus;
+    }
+    else if (bonusValue === 'I love you Rafi') {
+         const secondBonus = convertedBalance  + 1000;
+
+         document.getElementById('main-balance').innerText = secondBonus;
+        
+    }
+
+    else{
+        alert("Invalid Coupon")
+    }
+});
+
+
+// bill section
+document.getElementById('bill-btn').addEventListener('click',function(event){
+    event.preventDefault();
+    const amount =document.getElementById('bill-amount').value;
+    const convertedAmount = parseFloat(amount);
+    const pin = document.getElementById('bill-password').value;
+     const convertedPin = parseInt(pin);
+
+     const mainBalance = document.getElementById("main-balance").innerText;
+     
+     const convertedMainBalance = parseFloat(mainBalance);
+     
+     const number = document.getElementById('bill-number').value;
+     console.log(typeof number)
+    
+     if(number === "01618802042" ){
+        if(convertedPin === 1234){
+            const sum=convertedMainBalance - convertedAmount;
+            document.getElementById("main-balance").innerText = sum; 
+            console.log(typeof sum);
+        }
+        else{
+            alert("Invalid Password");
+        }
+
+    
+            
+        
+    }
+    else{
+        alert("Invalid Number")
+    }
+
+});
--- script/toggle.js
+++ script/toggle.js
@@ -1,3 +1,13 @@
+document.getElementById('cash-out-form').style.display="none";
+
+document.getElementById('add-money-form').style.display="none";
+
+document.getElementById('transfer-money-form').style.display="none";
+
+document.getElementById('bill-form').style.display = 'none';
+
+
+
 document.getElementById('add-money-box').addEventListener('click', function(){
     document.getElementById('cash-out-form').style.display="none";
 
@@ -11,7 +21,28 @@ document.getElementById('add-money-box').addEventListener('click', function(){
     const cashOutBox = document.getElementById('cashout-box');
     
   cashOutBox.classList.remove("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
+
+  const transfer = document.getElementById('transfer-box');
+
+  transfer.classList.remove("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
+
+  document.getElementById('transfer-money-form').style.display="none";
+
+  const bonusCoupon = document.getElementById('bonus-box');
+
+   bonusCoupon.classList.remove("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
+
+   document.getElementById('bonus-form').style.display = 'none';
+
+   const billBox = document.getElementById('bill-box');
+
+ billBox.classList.remove("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
+
+ document.getElementById('bill-form').style.display = 'none';
+
   });
+
+
   
 
 
@@ -33,6 +64,136 @@ document.getElementById('cashout-box').addEventListener('click',function(){
     
   cashOutBox.classList.add("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
 
+  const transfer = document.getElementById('transfer-box');
+
+  transfer.classList.remove("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
+
+  document.getElementById('transfer-money-form').style.display="none";
+
+  document.getElementById('bonus-form').style.display = 'none';
+
+  const bonusCoupon = document.getElementById('bonus-box');
+
+   bonusCoupon.classList.remove("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
+
+   const billBox = document.getElementById('bill-box');
+ billBox.classList.remove("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
+
+ document.getElementById('bill-form').style.display = 'none';
+
+
+});
+
+
+// transfer
+
+document.getElementById('transfer-box').addEventListener('click',function(){
+    document.getElementById('add-money-form').style.display = 'none';
+    document.getElementById('cash-out-form').style.display = 'none';
+
+    document.getElementById('transfer-money-form').style.display = 'block';
+    document.getElementById('bonus-form').style.display = 'none';
+
+   const addMoney = document.getElementById('add-money-box');
+
+   addMoney.classList.remove("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
+
+   const cashOut = document.getElementById('cashout-box');
+   cashOut.classList.remove("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
+
+   const transfer = document.getElementById('transfer-box');
+
+   transfer.classList.add("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
+
+   const bonusCoupon = document.getElementById('bonus-box');
+
+   bonusCoupon.classList.remove("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
+
+   const billBox = document.getElementById('bill-box');
+ billBox.classList.remove("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
+
+ document.getElementById('bill-form').style.display = 'none';
+
+    
+});
+
+// coupon
+document.getElementById('bonus-box').addEventListener('click',function(){
+    
+    document.getElementById('add-money-form').style.display = 'none';
+    document.getElementById('cash-out-form').style.display = 'none';
+
+    document.getElementById('transfer-money-form').style.display = 'none';
+   
+    document.getElementById('bonus-form').style.display = 'block';
+
+    
+
+
+
+   const addMoney = document.getElementById('add-money-box');
+
+
+   addMoney.classList.remove("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
+
+   const cashOut = document.getElementById('cashout-box');
+   cashOut.classList.remove("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
+
+   const transfer = document.getElementById('transfer-box');
+
+   transfer.classList.remove("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
+
+
+   const bonusCoupon = document.getElementById('bonus-box');
+
+   bonusCoupon.classList.add("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
+
+   bonusCoupon.classList.add("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
+
+ const billBox = document.getElementById('bill-box');
+ billBox.classList.remove("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
+
+ document.getElementById('bill-form').style.display = 'none';
+
+
+})
+
+
+// bill box
+
+document.getElementById('bill-box').addEventListener('click',function(){
+    
+  document.getElementById('add-money-form').style.display = 'none';
+  document.getElementById('cash-out-form').style.display = 'none';
+
+  document.getElementById('transfer-money-form').style.display = 'none';
+ 
+  document.getElementById('bonus-form').style.display = 'none';
+  document.getElementById('bill-form').style.display = 'block';
+
+  
+
+
+
+ const addMoney = document.getElementById('add-money-box');
+
+
+ addMoney.classList.remove("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
+
+ const cashOut = document.getElementById('cashout-box');
+ cashOut.classList.remove("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
+
+ const transfer = document.getElementById('transfer-box');
+
+ transfer.classList.remove("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
+
+
+ const bonusCoupon = document.getElementById('bonus-box');
+
+ bonusCoupon.classList.remove("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
+
+ const billBox = document.getElementById('bill-box');
+ billBox.classList.add("box-border","border-[#0874f2]","bg-[#0874f2]","bg-opacity-5","border-2");
 
 
 })
